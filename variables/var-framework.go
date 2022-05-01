6631c011481e749742cb507c7cb2e980e09b3bd0
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-01 21:24:46 +0200

    Variables initialized.

--- variables/var-framework.go
+++ variables/var-framework.go
@@ -1,15 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 var (
-	name   string
-	course string
-	module int
-	clip   int
+	name   = "Nigel Poulton"
+	course = "Getting started with Kubernetes"
+	module = 4
+	clip   = 2
 )
 
 func main() {
 	fmt.Println("Name and course are set to", name, "and", course, ".")
 	fmt.Println("Module and clip are set to", module, "and", clip, ".")
+	fmt.Println("Name is of type", reflect.TypeOf(name))
+	fmt.Println("Module is of type", reflect.TypeOf(module))
 }
