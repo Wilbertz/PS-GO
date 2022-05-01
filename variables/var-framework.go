4e63aed8aacff3cf2776d9162a45e39f66ac0d61
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-01 22:01:08 +0200

    Variables now declared locally.

--- variables/var-framework.go
+++ variables/var-framework.go
@@ -3,18 +3,22 @@ package main
 import (
 	"fmt"
 	"reflect"
-)
-
-var (
-	name   = "Nigel Poulton"
-	course = "Getting started with Kubernetes"
-	module = 4
-	clip   = 2
+	"strconv"
 )
 
 func main() {
+	name := "Nigel Poulton"
+	course := "Getting started with Kubernetes"
+	module := "4" // Needs to be an integer
+	clip := 2     // Needs to be an integer
+
 	fmt.Println("Name and course are set to", name, "and", course, ".")
 	fmt.Println("Module and clip are set to", module, "and", clip, ".")
 	fmt.Println("Name is of type", reflect.TypeOf(name))
-	fmt.Println("Module is of type", reflect.TypeOf(module))
+
+	iModule, err := strconv.Atoi(module)
+	if err == nil {
+		total := iModule + clip
+		fmt.Println("Module plus clip equals", total)
+	}
 }
