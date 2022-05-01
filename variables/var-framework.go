79f3151a9433f1ee632cb0a5ec1fc8832f2d8cd5
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-01 21:17:56 +0200

    Variables added.

---
+++ variables/var-framework.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+var (
+	name   string
+	course string
+	module int
+	clip   int
+)
+
+func main() {
+	fmt.Println("Name and course are set to", name, "and", course, ".")
+	fmt.Println("Module and clip are set to", module, "and", clip, ".")
+}
