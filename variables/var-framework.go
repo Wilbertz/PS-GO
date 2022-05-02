a4b82c2db2b1fa4f9b297587af8f2dd5c228b7be
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-02 21:43:47 +0200

    Call by reference.

--- variables/var-framework.go
+++ variables/var-framework.go
@@ -7,4 +7,13 @@ func main() {
 	course := "Getting started with Kubernetes"
 
 	fmt.Println("\nHi", name, "your current course is", course)
+	updateCourse(&course)
+
+	fmt.Println("Your are currently watching", course)
+}
+
+func updateCourse(course *string) string {
+	*course = "Getting Started with Docker"
+	fmt.Println("Update course to", *course)
+	return *course
 }
