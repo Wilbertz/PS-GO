e00f8629298c49b63692055b460172c419a6b704
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-03 20:04:31 +0200

    Converter function added.

---
+++ functions/convert.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	author := "nigel poulton"
+	course := "getting started with kubernetes"
+
+	fmt.Println(converter(author, course))
+}
+
+func converter(author string, course string) (a, c string) {
+	author = strings.ToUpper(author)
+	course = strings.ToUpper(course)
+	return author, course
+}
