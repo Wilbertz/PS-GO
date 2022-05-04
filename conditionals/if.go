aaf3ddc48bead98fe988e37d6147ce56761472d9
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-04 22:10:26 +0200

    Logic reversed.

--- conditionals/if.go
+++ conditionals/if.go
@@ -3,8 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	dddLengthMins := 275
-	cawLengthMins := 30
+	dddLengthMins := 60
+	cawLengthMins := 120
 
 	if dddLengthMins > cawLengthMins {
 		fmt.Println("Docker Deep Dive is longer than Containers on AWS Wavelength")
