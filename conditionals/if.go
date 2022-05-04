bdc68c20b3f1754c92dd68afd047ae970702be91
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-04 22:11:19 +0200

    Logic reversed.

--- conditionals/if.go
+++ conditionals/if.go
@@ -7,10 +7,10 @@ func main() {
 	cawLengthMins := 120
 
 	if dddLengthMins > cawLengthMins {
-		fmt.Println("Docker Deep Dive is longer than Containers on AWS Wavelength")
+		fmt.Println("Docker Deep Dive is longer than Containers on AWS Wavelength.")
 	} else if dddLengthMins == cawLengthMins {
 		fmt.Println("Both courses are the same length.")
 	} else {
-		fmt.Println("Containers on AWS Wavelength must be longer than Docker Deep Dive")
+		fmt.Println("Containers on AWS Wavelength must be longer than Docker Deep Dive.")
 	}
 }
