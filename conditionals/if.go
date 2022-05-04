d41302c721603b4dd0e5399b2b7e8bc0b0d01b27
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-04 22:00:54 +0200

    If statement added.

--- conditionals/if.go
+++ conditionals/if.go
@@ -1,1 +1,12 @@
-package conditionals
+package main
+
+import "fmt"
+
+func main() {
+	dddLengthMins := 275
+	cawLengthMins := 30
+
+	if dddLengthMins > cawLengthMins {
+		fmt.Println("Docker Deep Dive is longer than Containers on AWS Wavelength")
+	}
+}
