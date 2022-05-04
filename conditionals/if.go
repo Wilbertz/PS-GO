89f56442eea8e6b753fc6345cfc2a334df86fc63
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-04 22:06:38 +0200

    Additional logic.

--- conditionals/if.go
+++ conditionals/if.go
@@ -8,5 +8,9 @@ func main() {
 
 	if dddLengthMins > cawLengthMins {
 		fmt.Println("Docker Deep Dive is longer than Containers on AWS Wavelength")
+	} else if dddLengthMins == cawLengthMins {
+		fmt.Println("Both courses are the same length.")
+	} else {
+		fmt.Println("Containers on AWS Wavelength must be longer than Docker Deep Dive")
 	}
 }
