6ad75d07fbeaa949b8ea63080c3d0b78c326836d
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-05 20:42:48 +0200

    Variable declaration moved.

--- conditionals/if.go
+++ conditionals/if.go
@@ -3,10 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	dddLengthMins := 60
-	cawLengthMins := 120
-
-	if dddLengthMins > cawLengthMins {
+	if dddLengthMins, cawLengthMins := 275, 30; dddLengthMins > cawLengthMins {
 		fmt.Println("Docker Deep Dive is longer than Containers on AWS Wavelength.")
 	} else if dddLengthMins == cawLengthMins {
 		fmt.Println("Both courses are the same length.")
