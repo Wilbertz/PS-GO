a52d873d6e3cdf74a7d02b93136fa5b13fc99c1a
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-06 21:28:59 +0200

    Improved timer version.

--- loops/self-destruct.go
+++ loops/self-destruct.go
@@ -6,7 +6,11 @@ import (
 )
 
 func main() {
-	for timer := 10; timer > 0; timer-- {
+	for timer := 10; timer >= 0; timer-- {
+		if timer == 0 {
+			fmt.Println("Boom!")
+			break
+		}
 		fmt.Println(timer)
 		time.Sleep(1 * time.Second)
 	}
