0aaaf1b69469d15c1a9f52f9df0f26369c763d1a
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-07 22:14:17 +0200

    Loop added.

--- loops/for-range.go
+++ loops/for-range.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
 
+	coursesInProg := []string{
+		"Docker & Kubernetes",
+		"Docker Networking",
+		"Getting Started with Kubernetes",
+		"Kubernetes Deep Dive"}
+
+	for _, i := range coursesInProg {
+		fmt.Println(i)
+	}
 }
