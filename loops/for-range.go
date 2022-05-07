ec1d0d26761ceec8c3ffc5ee01789a0a80126865
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-07 22:34:35 +0200

    Nested loops.

--- loops/for-range.go
+++ loops/for-range.go
@@ -10,7 +10,16 @@ func main() {
 		"Getting Started with Kubernetes",
 		"Kubernetes Deep Dive"}
 
+	coursesCompleted := []string{
+		"Docker & Kubernetes",
+		"Docker Deep Dive"}
+
 	for i, data := range coursesInProg {
 		fmt.Println(i, data)
+		for _, j := range coursesCompleted {
+			if data == j {
+				fmt.Println("Hey we found a clash.", j, "is in both lists")
+			}
+		}
 	}
 }
