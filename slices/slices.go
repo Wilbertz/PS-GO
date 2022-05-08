f042cab4f7909d43420fd12c056cdfd2c042358c
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-08 19:23:03 +0200

    Slice added.

--- slices/slices.go
+++ slices/slices.go
@@ -1,5 +1,9 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	courses := make([]string, 5, 10)
+
+	fmt.Printf("Length of slice is %d and capacity is %d\n", len(courses), cap(courses))
 }
