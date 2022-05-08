5d0e76533d88eab9f78e1987ca7eca4c1adb9937
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-08 19:25:12 +0200

    Data added.

--- slices/slices.go
+++ slices/slices.go
@@ -5,5 +5,9 @@ import "fmt"
 func main() {
 	courses := make([]string, 5, 10)
 
+	courses[0] = "Docker & Kubernetes"
+	courses[1] = "Getting started with Docker"
+	courses[2] = "Getting started with Kubernetes"
+
 	fmt.Printf("Length of slice is %d and capacity is %d\n", len(courses), cap(courses))
 }
