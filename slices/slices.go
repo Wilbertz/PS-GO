683ba74f318463f3103d6397d1dfb27155b3b18b
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-08 19:28:03 +0200

    Code reformatted.

--- slices/slices.go
+++ slices/slices.go
@@ -3,11 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	courses := make([]string, 5, 10)
-
-	courses[0] = "Docker & Kubernetes"
-	courses[1] = "Getting started with Docker"
-	courses[2] = "Getting started with Kubernetes"
+	courses := []string{"Docker & Kubernetes",
+		"Getting started with Docker",
+		"Getting started with Kubernetes"}
 
 	fmt.Printf("Length of slice is %d and capacity is %d\n", len(courses), cap(courses))
+
+	fmt.Println(courses)
 }
