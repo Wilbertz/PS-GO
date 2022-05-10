43b776033b2f725d0497c7fe025da5fa49ebace6
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-10 18:20:10 +0200

    Slice expanded.

--- slices/append.go
+++ slices/append.go
@@ -7,4 +7,9 @@ import (
 func main() {
 	mySlice := make([]int, 1, 4)
 	fmt.Printf("Length starts out as %d with capacity of %d\n", len(mySlice), cap(mySlice))
+
+	for i := 1; i < 17; i++ {
+		mySlice = append(mySlice, i)
+		fmt.Printf("Length starts out as %d with capacity of %d\n", len(mySlice), cap(mySlice))
+	}
 }
