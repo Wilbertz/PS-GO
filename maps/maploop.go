ee2a57bbf064ab1c7ef5d46703e3e77902065788
Harald Wilbertz <harald.wilbertz@gmx.de>
2022-05-10 19:34:47 +0200

    Iteration added.

---
+++ maps/maploop.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	testMap := map[string]int{
+		"A": 1,
+		"B": 2,
+		"C": 3,
+		"D": 4,
+		"E": 5,
+		"F": 6,
+		"G": 7,
+		"H": 8,
+		"I": 9,
+	}
+	for mapKey, mapVal := range testMap {
+		fmt.Printf("Key is: %v Value is %v\n", mapKey, mapVal)
+	}
+}
